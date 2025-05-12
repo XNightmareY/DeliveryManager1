7fa5f78e629ca4340e0422edafdbef5bc9398bde
3291922-16 <3291922-16@A401PWS12>
2025-05-12 10:06:22 +0300

    Create class "Delivery"

---
+++ Delivery.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryManager
+{
+    public enum DeliveryStatus
+    {
+        Новый,
+        В_пути,
+        Доставлен
+    }
+
+    public class Delivery
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public DeliveryStatus Status { get; set; }
+        public Delivery(string customerName, string address, DateTime deliveryDate)
+        {
+            CustomerName = customerName;
+            Address = address;
+            DeliveryDate = deliveryDate;
+            Status = DeliveryStatus.Новый;
+        }
+        public void UpdateStatus(DeliveryStatus newStatus)
+        {
+            Status = newStatus;
+        }
+    }
+}
